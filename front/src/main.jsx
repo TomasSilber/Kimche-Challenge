9f8b7821216a95b44880a2e816e707b80b44fe29
TomasSilber <tomi.silber@hotmail.com>
2024-03-31 19:49:06 -0300

    Uri arreglada

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:4000/'
+    uri: 'https://rickandmorty-tomassilbergleit.vercel.app'
   })
 });
 
